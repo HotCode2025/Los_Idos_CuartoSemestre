3d8860a23dca6837c105823b9c165bc35cb47eea
Gabriel Soria <gabrieledgardosoria26@gmail.com>
2026-08-20 21:55:56 +0000

    Se agrega clase 1 Ecommerce basico

---
+++ Javascript/Ecommerce2026/Client/Js/products.js
@@ -0,0 +1,89 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Buzo gris",
+        price: 30000,
+        quanty: 1,
+        img: "/Client/Media/Buzo gris.png",
+
+
+    },
+    {
+        id: 2,
+        productName: "Buzo negro",
+        price: 30000,
+        quanty: 1,
+        img: "/Client/Media/Buzo negro.png",
+    },
+    {
+        id: 3,
+        productName: "Cartera de cuero marron",
+        price: 50000,
+        quanty: 1,
+        img: "/Client/Media/Cartera de cuero marron.png",
+    },
+    {
+        img: 4,
+        productName: "Cartera de cuero negra",
+        price: 50000,
+        quanty: 1,
+        img:"/Client/Media/Cartera de cuero negra.png",
+    },
+    {
+        img: 5,
+        productName: "Falda de cuero negra",
+        price: 30000,
+        quanty: 1,
+        img:"/Client/Media/Falda de cuero negra.png",
+
+    },
+    {
+        img: 6,
+        productName: "Pantalon corto de hombre",
+        price: 20000,
+        quanty: 1,
+        img:"/Client/Media/Pantalon corto de hombre.png",
+    },
+    {
+        img: 7,
+        productName: "Pantalon corto hombre",
+        price: 20000,
+        quanty: 1,
+        img:"/Client/Media/Pantalon corto hombre.png",
+    },
+    {
+        img: 8,
+        productName: "Pantalon de jean claro",
+        price: 30000,
+        quanty: 1,
+        img:"/Client/Media/Pantalon de jean claro.png",
+    },
+    {
+        img: 9,
+        productName: "Pantalon de jean oscuro",
+        price: 30000,
+        quanty: 1,
+        img:"/Client/Media/Pantalon de jean oscuro.png",
+    },
+    {
+        img: 10,
+        productName: "Pantalon Oxford mujer",
+        price: 35000,
+        quanty: 1,
+        img:"/Client/Media/Pantalon Oxford mujer.png",
+    },
+    {
+        img: 11,
+        productName: "Parka naranja",
+        price: 70000,
+        quanty: 1,
+        img:"/Client/Media/Parka naranja.png",
+    },
+    {
+        img: 12,
+        productName: "Vestido negro mujer",
+        price: 30000,
+        quanty: 1,
+        img:"/Client/Media/Vestido negro mujer.png",
+    },
+]
