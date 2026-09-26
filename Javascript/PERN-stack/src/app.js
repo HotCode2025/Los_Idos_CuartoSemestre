6ba769305a6d7ce086ec73fb7108070fea1f405f
Lucia Farina <luufarina@gmail.com>
2026-09-25 21:33:05 -0300

    Fin parte 1 stack pern

--- Javascript/PERN-stack/src/app.js
+++ Javascript/PERN-stack/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import tareasRoutes from "./router/tareas.routes.js";
+import authRoutes from "./router/auth.routes.js";
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) =>
   res.json({ message: "Bienvenidos a mi proyecto" }),
 );
+app.use('/api', tareasRoutes);
+app.use('/api', authRoutes);
 
 app.use((err, req, res, next) => {
   res.status(500).json({
